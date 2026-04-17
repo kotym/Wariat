f40b27dd74c8174a07c017ca3a00bfd625ce5d3e
kotym <kosetka.tymoteusz@gmail.com>
2026-04-17 21:34:31 +0200

    Motors basic code.

--- WariatCore2/src/Motors.cpp
+++ WariatCore2/src/Motors.cpp
@@ -1,1 +1,32 @@
 #include "Motors.hpp"
+#include "hFramework.h"
+
+Motors::Motors()
+{
+    hMot1.setEncoderPolarity(Polarity::Reversed);
+    hMot1.setMotorPolarity(Polarity::Normal);
+    hMot2.setEncoderPolarity(Polarity::Reversed);
+    hMot2.setMotorPolarity(Polarity::Normal);
+    
+}
+
+Motors::~Motors()
+{
+
+}
+
+void Motors::MoveForeward(float distanceCm)
+{
+    const float rotations = distanceCm / wheelCircumferenceCm;
+    const int32_t encoderClicks = rotations * encoderClicksPerFullRotation;
+    hMot1.rotRel(encoderClicks, 300);
+    hMot2.rotRel(encoderClicks, 300);
+}
+
+void Motors::Rotate(float angleRad)
+{
+    const float wheelDistanceToTravel = M_PI * angleRad;
+    const int32_t encoderClicks = wheelDistanceToTravel / wheelCircumferenceCm * encoderClicksPerFullRotation;
+    hMot1.rotRel(-encoderClicks, 300);
+    hMot2.rotRel(encoderClicks, 300);
+}
--- WariatCore2/src/Motors.hpp
+++ WariatCore2/src/Motors.hpp
@@ -2,6 +2,16 @@
 
 class Motors
 {
+    Motors();
+    ~Motors();
 
+public:
+    void MoveForeward(float distanceCm);
+    void Rotate(float angleRad);
 
+private:
+    float encoderClicksPerFullRotation = 720;
+    float wheelDiameterCm = 5.6; //cm
+    float wheelCircumferenceCm = 17.6; //cm
+    float wheelbaseCm = 10;
 };
